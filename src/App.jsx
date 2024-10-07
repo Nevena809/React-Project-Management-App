5438d9d2320d65fb651c1492220fcaed148cfa18
Nevena <nevena.milojevic@dev.telnet.rs>
2024-10-07 11:45:30 +0200

    add-project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import { useState } from "react";
+import AddProject from "./components/AddProject";
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+
 function App() {
+  const [showNewProject, setShowNewProject] = useState(false);
+
+  function handleShowNewProjects() {
+    setShowNewProject(true);
+  }
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <AddProject click={handleShowNewProjects}></AddProject>
+      {showNewProject ? (
+        <NewProject></NewProject>
+      ) : (
+        <NoProjectSelected></NoProjectSelected>
+      )}
+    </main>
   );
 }
 
